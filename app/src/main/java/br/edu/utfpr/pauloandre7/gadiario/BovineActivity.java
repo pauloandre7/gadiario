ca8ff1b139288c55d98f5c8f3e31a2e15fc00ce4
Paulo André <andrepaulos.2001@gmail.com>
2026-03-27 16:53:55 -0300

    feat: introduces the Checkbox element in Clear and Save logic
    This commit:
    - Makes the checkbox unCheck the selection with Clear button click
    - Makes the Toast expose the string "Is Vaccinated" or "Is not Vaccinated"
    if the checkbox is check or not checked

--- app/src/main/java/br/edu/utfpr/pauloandre7/gadiario/BovineActivity.java
+++ app/src/main/java/br/edu/utfpr/pauloandre7/gadiario/BovineActivity.java
@@ -2,14 +2,19 @@ package br.edu.utfpr.pauloandre7.gadiario;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BovineActivity extends AppCompatActivity {
 
     private EditText editTextTag, editTextName, editTextDate;
+    private final List<CheckBox> checkBoxVaccines = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +24,18 @@ public class BovineActivity extends AppCompatActivity {
         editTextTag = findViewById(R.id.editTextTag);
         editTextName = findViewById(R.id.editTextName);
         editTextDate = findViewById(R.id.editTextDate);
+        checkBoxVaccines.add(findViewById(R.id.checkBox_vaccines_op1));
+        checkBoxVaccines.add(findViewById(R.id.checkBox_vaccines_op2));
     }
 
     public void cleanFields(View view){
         editTextTag.setText(null);
         editTextName.setText(null);
         editTextDate.setText(null);
+        checkBoxVaccines.get(0).setChecked(false);
 
         Toast.makeText(this,
-                R.string.cad_bov_toast_fields_cleaned, Toast.LENGTH_LONG).show();
+                R.string.reg_bov_toast_fields_cleaned, Toast.LENGTH_LONG).show();
     }
 
     public void saveValues(View view){
@@ -37,7 +45,7 @@ public class BovineActivity extends AppCompatActivity {
 
         if(tag == null || tag.trim().isEmpty()){
             Toast.makeText(this,
-                    R.string.cad_bov_toast_text_tagMissing, Toast.LENGTH_LONG).show();
+                    R.string.reg_bov_toast_text_tagMissing, Toast.LENGTH_LONG).show();
 
             editTextName.requestFocus();
             return;
@@ -45,7 +53,7 @@ public class BovineActivity extends AppCompatActivity {
 
         if(name == null || name.trim().isEmpty()){
             Toast.makeText(this,
-                            R.string.cad_bov_toast_text_nameMissing, Toast.LENGTH_LONG).show();
+                            R.string.reg_bov_toast_text_nameMissing, Toast.LENGTH_LONG).show();
 
             editTextName.requestFocus();
             return;
@@ -53,15 +61,26 @@ public class BovineActivity extends AppCompatActivity {
 
         if(date == null || date.trim().isEmpty()){
             Toast.makeText(this,
-                    R.string.cad_bov_toast_text_birthMissing, Toast.LENGTH_LONG).show();
+                    R.string.reg_bov_toast_text_birthMissing, Toast.LENGTH_LONG).show();
 
             editTextName.requestFocus();
             return;
         }
 
-        String resultFields = getString(R.string.cad_bov_toast_text_tagValue)+tag+"\n"
-                            +getString(R.string.cad_bov_toast_text_nameValue)+name+"\n"
-                            +getString(R.string.cad_bov_toast_text_birthValue)+date;
+        boolean isVaccinated = false;
+        // For-each em java pega cada elemento que tiver no array e guarda na variável option
+        // Cada elemento é considerado como um ciclo, então facilita minha vida na hora de percorrer array.
+        for (CheckBox option : checkBoxVaccines){
+            isVaccinated = option.isChecked();
+            if (isVaccinated == true){
+                break;
+            }
+        }
+
+        String resultFields = getString(R.string.reg_bov_toast_text_tagValue)+tag+"\n"
+                            +getString(R.string.reg_bov_toast_text_nameValue)+name+"\n"
+                            +getString(R.string.reg_bov_toast_text_birthValue)+date+"\n"
+                            +(isVaccinated ? getString(R.string.reg_bov_toast_text_isVaccinated) : getString(R.string.reg_bov_toast_text_notVaccinated));
         Toast.makeText(this,
                         resultFields, Toast.LENGTH_LONG).show();
     }
