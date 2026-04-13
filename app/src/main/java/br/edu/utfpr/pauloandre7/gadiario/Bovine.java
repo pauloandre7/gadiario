32bd3e949eb1231157a28402954fd6e9c4f03bcb
Paulo André <andrepaulos.2001@gmail.com>
2026-04-13 02:24:28 -0300

    feat: adds basic Bovine entity to store data
    This entity its only implemented to finish class homework,
    but will be modified to final project.
    
    this commits:
    - adds new entity Bovine
    - adds enum animalSex
    - adds custom toString() method

---
+++ app/src/main/java/br/edu/utfpr/pauloandre7/gadiario/Bovine.java
@@ -0,0 +1,81 @@
+package br.edu.utfpr.pauloandre7.gadiario;
+
+import java.util.List;
+
+public class Bovine {
+
+    private String tag;
+    private String name;
+    private String date;
+    private AnimalSex animalSex;
+    private String breed;
+    private List<String> vaccines;
+
+    public Bovine(String tag, String name, String date, AnimalSex animalSex, String breed, List<String> vaccines) {
+        this.tag = tag;
+        this.name = name;
+        this.date = date;
+        this.animalSex = animalSex;
+        this.breed = breed;
+        this.vaccines = vaccines;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public AnimalSex getAnimalSex() {
+        return animalSex;
+    }
+
+    public void setAnimalSex(AnimalSex animalSex) {
+        this.animalSex = animalSex;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public List<String> getVaccines() {
+        return vaccines;
+    }
+
+    public void setVaccines(List<String> vaccines) {
+        this.vaccines = vaccines;
+    }
+
+    @Override
+    public String toString() {
+        return  tag + "\n" +
+                name + "\n" +
+                date + "\n" +
+                animalSex + "\n" +
+                breed + "\n" +
+                vaccines.get(0) + "\n" +
+                vaccines.get(1);
+    }
+}
