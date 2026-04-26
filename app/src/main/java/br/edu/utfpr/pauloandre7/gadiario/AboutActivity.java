e129e02fd8dccdc3b6d19f43d0fa3c39f3813bae
Paulo André <andrepaulos.2001@gmail.com>
2026-04-25 21:30:38 -0300

    feat: add about activity

---
+++ app/src/main/java/br/edu/utfpr/pauloandre7/gadiario/AboutActivity.java
@@ -0,0 +1,19 @@
+package br.edu.utfpr.pauloandre7.gadiario;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        setTitle(getString(R.string.about_class_text_about));
+
+    }
+
+
+}
