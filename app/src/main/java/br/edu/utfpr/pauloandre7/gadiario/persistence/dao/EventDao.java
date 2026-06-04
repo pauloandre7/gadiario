ff8dc7387862bdcc64e77f2bf32e591aa2945ecf
pauloandre7 <andrepaulos.2001@gmail.com>
2026-06-04 19:11:13 -0300

    feat: create EventDao interface for Room persistence
    This commit:
    - adds insert, Delete and update methods;
    - adds custom queries

---
+++ app/src/main/java/br/edu/utfpr/pauloandre7/gadiario/persistence/dao/EventDao.java
@@ -0,0 +1,37 @@
+package br.edu.utfpr.pauloandre7.gadiario.persistence.dao;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+import br.edu.utfpr.pauloandre7.gadiario.models.Event;
+
+@Dao
+public interface EventDao {
+
+    @Insert
+    long insert(Event event);
+
+    @Delete
+    int delete(Event event);
+
+    @Update
+    int update(Event event);
+
+    @Query("SELECT * FROM event ORDER BY date ASC")
+    List<Event> queryAllAscending();
+
+    @Query("SELECT * FROM event ORDER BY date DESC")
+    List<Event> queryAllDownward();
+
+    @Query("SELECT * FROM event WHERE id=:id")
+    List<Event> queryAllAscending(long id);
+
+    //
+    @Query("SELECT * FROM event WHERE idBovine=:idBovine ORDER BY date DESC")
+    List<Event> queryEventsByBovine(long idBovine);
+}
