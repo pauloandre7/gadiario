be94f6069d6bb6069067c2437af3e771b0524e91
pauloandre7 <andrepaulos.2001@gmail.com>
2026-06-05 01:44:23 -0300

    feat: implement database integration and spinner loading in EventActivity

--- app/src/main/java/br/edu/utfpr/pauloandre7/gadiario/ui/event/EventActivity.java
+++ app/src/main/java/br/edu/utfpr/pauloandre7/gadiario/ui/event/EventActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ScrollView;
 import android.widget.Spinner;
@@ -14,32 +15,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.List;
+
 import br.edu.utfpr.pauloandre7.gadiario.R;
+import br.edu.utfpr.pauloandre7.gadiario.models.Bovine;
 import br.edu.utfpr.pauloandre7.gadiario.models.Event;
 import br.edu.utfpr.pauloandre7.gadiario.models.EventType;
+import br.edu.utfpr.pauloandre7.gadiario.models.Pasture;
+import br.edu.utfpr.pauloandre7.gadiario.persistence.GadiarioDatabase;
 import br.edu.utfpr.pauloandre7.gadiario.utils.AlertUtils;
 
 public class EventActivity extends AppCompatActivity {
 
-    // Constantes para Intent
-    public static final String KEY_ID = "KEY_ID";
-    public static final String KEY_BOVINE_ID = "KEY_BOVINE_ID";
-    public static final String KEY_TYPE = "KEY_TYPE";
-    public static final String KEY_DATE = "KEY_DATE";
-    public static final String KEY_QTY_CALVES = "KEY_QTY_CALVES";
-    public static final String KEY_PASTURE_ORIGIN = "KEY_PASTURE_ORIGIN";
-    public static final String KEY_PASTURE_DESTINATION = "KEY_PASTURE_DESTINATION";
-    public static final String KEY_OBSERVATIONS = "KEY_OBSERVATIONS";
-
+    public static final String KEY_ID = "ID";
     public static final String KEY_MODE = "MODE";
     public static final int MODE_NEW = 0;
     public static final int MODE_EDIT = 1;
 
-    // Atributos da UI
     private Spinner spinnerBovine, spinnerType, spinnerOriginPasture, spinnerDestinationPasture;
     private EditText editTextDate, editTextQtyCalves, editTextObservations;
 
-    // Atributo para verificar mudanças no objeto original (Edição)
+    private List<Bovine> bovines;
+    private List<Pasture> pastures;
+
     private Event eventOriginal;
     private int mode;
 
@@ -56,7 +54,7 @@ public class EventActivity extends AppCompatActivity {
         spinnerDestinationPasture   = findViewById(R.id.event_spinnerDestinationPasture);
         editTextObservations        = findViewById(R.id.event_etObservations);
 
-        // todo: Verificar - Adicionar adaptadores para os spinners de Bovinos e Pastos quando o DAO estiver pronto
+        loadSpinnersData();
 
         Intent intentOpen = getIntent();
         Bundle bundle = intentOpen.getExtras();
@@ -67,34 +65,68 @@ public class EventActivity extends AppCompatActivity {
             if (mode == MODE_NEW) {
                 setTitle(getString(R.string.event_reg_title));
             } else if (mode == MODE_EDIT) {
-                setTitle(getString(R.string.event_reg_title)); // todo: Verificar - Criar string específica para edição de evento se necessário
+                setTitle(getString(R.string.event_reg_title)); // Pode ajustar se tiver string de edição
 
-                int id = bundle.getInt(KEY_ID);
-                int idBovine = bundle.getInt(KEY_BOVINE_ID);
-                String typeStr = bundle.getString(KEY_TYPE);
-                String date = bundle.getString(KEY_DATE);
-                int qtyCalves = bundle.getInt(KEY_QTY_CALVES);
-                int idPastOrigin = bundle.getInt(KEY_PASTURE_ORIGIN);
-                int idPastDest = bundle.getInt(KEY_PASTURE_DESTINATION);
-                String observations = bundle.getString(KEY_OBSERVATIONS);
+                long id = bundle.getLong(KEY_ID);
+                GadiarioDatabase database = GadiarioDatabase.getInstance(this);
+                eventOriginal = database.getEventDao().queryById(id);
 
-                EventType typeEnum = EventType.valueOf(typeStr);
+                if (eventOriginal != null) {
+                    fillFields();
+                }
+            }
+        }
+    }
 
-                eventOriginal = new Event(id, idBovine, typeEnum, qtyCalves, idPastOrigin, idPastDest, date, observations);
+    private void loadSpinnersData() {
+        GadiarioDatabase database = GadiarioDatabase.getInstance(this);
+        bovines = database.getBovinesDao().queryAllAscending();
+        pastures = database.getPastureDao().queryAllAscending();
 
-                // Preencher campos
-                editTextDate.setText(date);
-                editTextQtyCalves.setText(String.valueOf(qtyCalves));
-                editTextObservations.setText(observations);
+        ArrayAdapter<Bovine> bovineAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, bovines);
+        bovineAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerBovine.setAdapter(bovineAdapter);
 
-                // todo: Verificar - Selecionar itens corretos nos Spinners baseados nos IDs recebidos
-                // spinnerType.setSelection(...) 
+        ArrayAdapter<Pasture> pastureAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, pastures);
+        pastureAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerOriginPasture.setAdapter(pastureAdapter);
+        spinnerDestinationPasture.setAdapter(pastureAdapter);
+    }
+
+    private void fillFields() {
+        editTextDate.setText(eventOriginal.getDate());
+        editTextQtyCalves.setText(String.valueOf(eventOriginal.getQtyCalves()));
+        editTextObservations.setText(eventOriginal.getObservation());
+
+        // Set type
+        EventType[] types = EventType.values();
+        for (int i = 0; i < types.length; i++) {
+            if (types[i] == eventOriginal.getType()) {
+                spinnerType.setSelection(i);
+                break;
+            }
+        }
+
+        // Set Bovine
+        for (int i = 0; i < bovines.size(); i++) {
+            if (bovines.get(i).getId() == eventOriginal.getIdBovine()) {
+                spinnerBovine.setSelection(i);
+                break;
+            }
+        }
+
+        // Set Pastures
+        for (int i = 0; i < pastures.size(); i++) {
+            if (pastures.get(i).getId() == eventOriginal.getIdPastureOrigin()) {
+                spinnerOriginPasture.setSelection(i);
+            }
+            if (pastures.get(i).getId() == eventOriginal.getIdPastureDestination()) {
+                spinnerDestinationPasture.setSelection(i);
             }
         }
     }
 
     public void clearFields() {
-        // Salva valores para o Undo
         final int bovinePos = spinnerBovine.getSelectedItemPosition();
         final int typePos = spinnerType.getSelectedItemPosition();
         final String date = editTextDate.getText().toString();
@@ -103,7 +135,6 @@ public class EventActivity extends AppCompatActivity {
         final int destPos = spinnerDestinationPasture.getSelectedItemPosition();
         final String obs = editTextObservations.getText().toString();
 
-        // Limpa
         spinnerBovine.setSelection(0);
         spinnerType.setSelection(0);
         editTextDate.setText(null);
@@ -127,46 +158,56 @@ public class EventActivity extends AppCompatActivity {
     }
 
     public void saveValues() {
-        // Validações básicas similares a BovineActivity
         String date = editTextDate.getText().toString();
         if (date.trim().isEmpty()) {
-            AlertUtils.showAlert(this, R.string.reg_bov_toast_text_birthMissing); // Reaproveitando string de data
+            AlertUtils.showAlert(this, R.string.event_reg_alertDialog_dateMissing);
             editTextDate.requestFocus();
             return;
         }
 
-        // Recuperar valores dos campos
-        // todo: Verificar - Implementar a lógica de pegar IDs dos spinners de Bovino e Pastos
-        int idBovine = 0; 
-        EventType type = EventType.values()[spinnerType.getSelectedItemPosition()];
+        if (spinnerBovine.getSelectedItem() == null) {
+            // Se não houver bovinos cadastrados
+            return;
+        }
+
+        long idBovine = ((Bovine) spinnerBovine.getSelectedItem()).getId();
+        EventType type = EventType.valueOf(spinnerType.getSelectedItem().toString().toUpperCase());
         int qtyCalves = Integer.parseInt(editTextQtyCalves.getText().toString());
-        int idPastOrigin = 0;
-        int idPastDest = 0;
+        
+        long idPastOrigin = 0;
+        if (spinnerOriginPasture.getSelectedItem() != null) {
+            idPastOrigin = ((Pasture) spinnerOriginPasture.getSelectedItem()).getId();
+        }
+
+        long idPastDest = 0;
+        if (spinnerDestinationPasture.getSelectedItem() != null) {
+            idPastDest = ((Pasture) spinnerDestinationPasture.getSelectedItem()).getId();
+        }
+
         String observations = editTextObservations.getText().toString();
 
-        if (mode == MODE_EDIT) {
-            // Verificação de mudanças similar a BovineActivity
-            if (idBovine == eventOriginal.getIdBovine() &&
-                type == eventOriginal.getType() &&
-                date.equals(eventOriginal.getDate()) &&
-                qtyCalves == eventOriginal.getQtyCalves() &&
-                observations.equals(eventOriginal.getObservation())) {
-                
-                setResult(RESULT_CANCELED);
-                finish();
+        Event event = new Event(idBovine, type, date, observations);
+        event.setQtyCalves(qtyCalves);
+        event.setIdPastureOrigin(idPastOrigin);
+        event.setIdPastureDestination(idPastDest);
+
+        GadiarioDatabase database = GadiarioDatabase.getInstance(this);
+
+        if (mode == MODE_EDIT && eventOriginal != null) {
+            event.setId(eventOriginal.getId());
+            // Verificação de igualdade se desejar implementar equals em Event
+            database.getEventDao().update(event);
+        } else {
+            long newId = database.getEventDao().insert(event);
+            if (newId <= 0) {
+                AlertUtils.showAlert(this, R.string.common_alertDialog_dbErrorInsert);
                 return;
             }
+            event.setId(newId);
         }
 
         Intent intentResult = new Intent();
-        intentResult.putExtra(KEY_BOVINE_ID, idBovine);
-        intentResult.putExtra(KEY_TYPE, type.toString());
-        intentResult.putExtra(KEY_DATE, date);
-        intentResult.putExtra(KEY_QTY_CALVES, qtyCalves);
-        intentResult.putExtra(KEY_PASTURE_ORIGIN, idPastOrigin);
-        intentResult.putExtra(KEY_PASTURE_DESTINATION, idPastDest);
-        intentResult.putExtra(KEY_OBSERVATIONS, observations);
-
+        intentResult.putExtra(KEY_ID, event.getId());
         setResult(RESULT_OK, intentResult);
         finish();
     }
